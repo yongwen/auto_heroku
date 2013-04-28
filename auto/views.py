c5fdc421a66babf2643a127fe9a8755615aa7c95
Yongwen <yongwen.xu@gmail.com>
2013-04-28 01:52:52 -1000

    implement auto deploy

--- auto/views.py
+++ auto/views.py
@@ -78,37 +78,35 @@ def work(request):
     except:
         pass
 
-    sshkey_cmd = "rm -rf .ssh; mkdir .ssh; ssh-keygen -q -N '' -f .ssh/id_rsa"
+    sshkey_cmd = "rm -rf /tmp/.ssh; mkdir /tmp/.ssh; ssh-keygen -q -N '' -f /tmp/.ssh/id_rsa"
     os.system(sshkey_cmd)
 
-    knownhosts_cmd = 'echo "|1|v2fAE9r+64rPyeKTVWZamQa95N8=|8cihuAGn19m0ljoDHJITbpNx618= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAu8erSx6jh+8ztsfHwkNeFr/SZaSOcvoa8AyMpaerGIPZDB2TKNgNkMSYTLYGDK2ivsqXopo2W7dpQRBIVF80q9mNXy5tbt1WE04gbOBB26Wn2hF4bk3Tu+BNMFbvMjPbkVlC2hcFuQJdH4T2i/dtauyTpJbD/6ExHR9XYVhdhdMs0JsjP/Q5FNoWh2ff9YbZVpDQSTPvusUp4liLjPfa/i0t+2LpNCeWy8Y+V9gUlDWiyYwrfMVI0UwNCZZKHs1Unpc11/4HLitQRtvuk0Ot5qwwBxbmtvCDKZvj1aFBid71/mYdGRPYZMIxq1zgP1acePC1zfTG/lvuQ7d0Pe0kaw==" > .ssh/known_hosts'
+    knownhosts_cmd = 'echo "|1|v2fAE9r+64rPyeKTVWZamQa95N8=|8cihuAGn19m0ljoDHJITbpNx618= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAu8erSx6jh+8ztsfHwkNeFr/SZaSOcvoa8AyMpaerGIPZDB2TKNgNkMSYTLYGDK2ivsqXopo2W7dpQRBIVF80q9mNXy5tbt1WE04gbOBB26Wn2hF4bk3Tu+BNMFbvMjPbkVlC2hcFuQJdH4T2i/dtauyTpJbD/6ExHR9XYVhdhdMs0JsjP/Q5FNoWh2ff9YbZVpDQSTPvusUp4liLjPfa/i0t+2LpNCeWy8Y+V9gUlDWiyYwrfMVI0UwNCZZKHs1Unpc11/4HLitQRtvuk0Ot5qwwBxbmtvCDKZvj1aFBid71/mYdGRPYZMIxq1zgP1acePC1zfTG/lvuQ7d0Pe0kaw==" > /tmp/.ssh/known_hosts'
     os.system(knownhosts_cmd)
 
     cloud = heroku.from_key(settings.MAKAHIKI_HEROKU_KEY)
-    file = open(".ssh/id_rsa.pub")
+    file = open("/tmp/.ssh/id_rsa.pub")
     for line in file:
         cloud.keys.add(line)
 
     print "setup ssh key"
 
     app_source = "git://github.com/yongwen/makahiki-min.git"
-    clone_cmd = "git clone %s git-tmp; " % app_source
+    clone_cmd = "git clone %s /tmp/git-tmp; " % app_source
     print "git clone"
     os.system(clone_cmd)
 
-    #push_cmd = 'cd git-tmp; git push git@heroku.com:%s.git master; curl -s -o /dev/null http://%s.herokuapp.com/init/' % (appname, appname)
+    push_cmd = 'cd /tmp/git-tmp; git push git@heroku.com:%s.git master; curl -s -o /dev/null http://%s.herokuapp.com/init/' % (appname, appname)
 
-    #cmd = 'echo "%s" > /tmp/push.sh' % (clone_cmd + push_cmd)
-    #os.system(cmd)
+    cmd = 'echo "%s" > /tmp/push.sh' % (push_cmd)
+    os.system(cmd)
 
-    push_cmd = 'cd git-tmp; git push git@heroku.com:%s.git master &' % (appname)
-    print "git push"
-    os.system(push_cmd)
+    #push_cmd = 'cd /tmp/git-tmp; git push git@heroku.com:%s.git master &' % (appname)
+    #print "git push"
+    #os.system(push_cmd)
 
     print "git clone and push"
-    #os.system("/bin/bash /tmp/push.sh &")
+    os.system("/bin/bash /tmp/push.sh &")
 
-    #print "clean up"
-    #os.system("rm -rf .ssh")
 
     return HttpResponseRedirect(reverse("home", args=()))
